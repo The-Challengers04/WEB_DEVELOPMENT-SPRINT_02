c077e1fba2b757c12562443567e9cab5b2d8b048
kaikyalvaro1708 <kaikialvaro57@gmail.com>
2023-05-13 19:39:08 -0300

    update map, sobre e empresa

--- Js/script.js
+++ Js/script.js
@@ -9,3 +9,61 @@ function menuBars(){
     })
 }
 menuBars();
+
+// Buscar locais no mapa
+function buscarPontos(){
+    let dropwdown = document.getElementById('escolhas')
+
+    let braile = (dropwdown.value == 'Deficiente Visual')
+    let cadeira = (dropwdown.value == 'Deficiente Físico (cadeirantes)')
+    let audicao = (dropwdown.value == 'Deficiente Auditivo')
+    const maps = document.getElementById('Maps')
+    let mapImg = document.getElementById('imgMap')
+    mapImg.style.maxWidth = "80vw"
+    mapImg.style.maxHeight = "60vh"
+
+    if (cadeira){
+        if(audicao){
+            if(braile){
+                //Cadeira Audicao Braile
+                mapImg.src = "../img/imgCadeiraAudicaoBraile.jpg"
+            }
+            else{
+                // Cadeira Audicao
+                mapImg.src = "../img/imgCadeiraAudicao.jpg"
+            }
+        }
+        else if (braile)
+        {
+            // Cadeira Braile
+            mapImg.src = "../img/imgCadeiraBraile.jpg"
+        }
+        else{
+            // Cadeira
+            mapImg.src = "../img/imgCadeira.jpg"
+        }
+    }
+    else if (audicao)
+    {
+        if (braile)
+        {
+            // Audicao Braile
+            mapImg.src = "../img/imgAudicaoBraile.jpg"
+        }
+        else
+        {
+            // Audicao
+            mapImg.src = "../img/imgAudicao.jpg"
+        }
+    }
+    else if (braile)
+    {
+        // Braile
+        mapImg.src = "../img/imgBraile.jpg"
+    }
+    else
+    {
+        // Mapa solo
+        mapImg.src = "../img/imgMapa.jpg"
+    }
+}
